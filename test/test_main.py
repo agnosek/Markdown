473ad196f7963fd4bc722193a6b5285ee46318ee
Agata Nosek <agatanosek01@gmail.com>
2022-01-28 19:13:18 +0100

    test

--- test/test_main.py
+++ test/test_main.py
@@ -1,13 +1,13 @@
-# import main
-#
-#
-# def test_decimal_to_binary():
-#     assert main.decimal_to_binary(144) == "10010000"
-#     assert main.decimal_to_binary(2) == "10"
-#     assert main.decimal_to_binary(1) == "1"
-#
-#
-# def test_factorial():
-#     assert main.factorial(4) == 24
-#     assert main.factorial(2) == 2
-#     assert main.factorial(1) == 1
+import main
+
+
+def test_decimal_to_binary():
+    assert main.decimal_to_binary(144) == "10010000"
+    assert main.decimal_to_binary(2) == "10"
+    assert main.decimal_to_binary(1) == "1"
+
+
+def test_factorial():
+    assert main.factorial(4) == 24
+    assert main.factorial(2) == 2
+    assert main.factorial(1) == 1
