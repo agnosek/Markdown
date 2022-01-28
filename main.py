f399e20f27397f78f8ac6903c8da2f0e98594cd9
agnosek <93132406+agnosek@users.noreply.github.com>
2022-01-28 16:14:14 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+def decimal_to_binary(n):
+    if n == 1:
+        return "1"
+    if n == 0:
+        return "0"
+    else:
+        return decimal_to_binary(n // 2) + str(n % 2)
+      
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return factorial(n-1) * n
